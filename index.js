1b0f1b4f26d8e59bc70dd33492154187208d3678
kb20025 <kb20025@kaipara.school.nz>
2024-10-20 21:58:06 +1300

    October 20 fix 3

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ctaBtn.classList.remove('pulse');
   });
 });
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+const navbar = document.querySelector('.navbar');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active'); // Toggle the 'active' class
+    navbar.classList.toggle('open'); // Toggle the 'open' class on the navbar for styling
+});
+
